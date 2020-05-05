23754df2b5b0993855a443b500991f3630fa11df
Gianluca Magni <gmagni@came.com>
2020-05-05 07:04:45 +0200

    save complete retry on out queue send

--- mqtt/out_queue.go
+++ mqtt/out_queue.go
@@ -1,6 +1,9 @@
 package mqtt
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
 func rangeOutQueue(connections Connections, retries Retries, outQueue <-chan OutData) {
 	for o := range outQueue {
@@ -8,21 +11,33 @@ func rangeOutQueue(connections Connections, retries Retries, outQueue <-chan Out
 			n, err := c.publish(o.packet.toByteSlice())
 			if err != nil {
 				log.Println("cannot write to", o.clientId, ":", err)
+			} else {
+				log.Println("published", n, "bytes to", o.clientId)
 			}
 			// if qos > 0 save for retry
 			if o.packet.QoS() > 0 {
-				var r Retry
-				r.clientId = o.clientId
-				r.packetIdentifier = o.packet.packetIdentifier
-				r.qos = o.packet.QoS()
-				err = retries.addRetry(r)
-				if err != nil {
-					log.Println("cannot save retry of", r.clientId, r.packetIdentifier)
-				}
+				waitForAck(o.clientId, o.packet, retries)
 			}
-			log.Println("published", n, "bytes to", o.clientId)
 		} else {
 			log.Println(o.clientId, "is not connected")
 		}
+	}
+}
+
+func waitForAck(clientId string, packet Packet, retries Retries) {
+	var r Retry
+	r.clientId = clientId
+	r.packetIdentifier = packet.packetIdentifier
+	r.qos = packet.QoS()
+	if r.qos == 1 {
+		r.ackStatus = WAIT_FOR_PUB_ACK
+	} else {
+		r.ackStatus = WAIT_FOR_PUB_REC
+	}
+	r.applicationMessage = packet.ApplicationMessage()
+	r.createdAt = time.Now()
+	err := retries.addRetry(r)
+	if err != nil {
+		log.Println("cannot save retry of", r.clientId, r.packetIdentifier)
 	}
 }
