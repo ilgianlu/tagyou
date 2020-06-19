9510f5e61b43de39a56dad2fe1e8ce05d49419b8
Gianluca Magni <gmagni@came.com>
2020-06-19 07:46:19 +0200

    more connections management methods

--- mqtt/connections.go
+++ mqtt/connections.go
@@ -3,3 +3,26 @@ package mqtt
 import "net"
 
 type Connections map[string]net.Conn
+
+func (c *Connections) Add(clientId string, conn net.Conn) {
+	(*c)[clientId] = conn
+}
+
+func (c *Connections) Exists(clientId string) (net.Conn, bool) {
+	if conn, ok := (*c)[clientId]; ok {
+		return conn, true
+	} else {
+		return nil, false
+	}
+}
+
+func (c *Connections) Close(clientId string) error {
+	if connection, ok := c.Exists(clientId); ok {
+		return connection.Close()
+	}
+	return nil
+}
+
+func (c *Connections) Remove(clientId string) {
+	delete(*c, clientId)
+}
