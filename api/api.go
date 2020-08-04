03dc142b36625b714d33f0c1860a7f07492e255d
Gianluca Magni <gmagni@came.com>
2020-08-04 08:47:18 +0200

    created api to post message to topic

--- api/api.go
+++ api/api.go
@@ -3,8 +3,12 @@ package api
 import (
 	"log"
 	"net/http"
+	"os"
+	"time"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	AuthController "github.com/ilgianlu/tagyou/api/controllers/auth"
+	MessageController "github.com/ilgianlu/tagyou/api/controllers/message"
 	SessionController "github.com/ilgianlu/tagyou/api/controllers/session"
 	"github.com/jinzhu/gorm"
 	"github.com/julienschmidt/httprouter"
@@ -15,17 +19,59 @@ func StartApi(httpPort string) {
 	if err != nil {
 		log.Fatal("failed to connect database")
 	}
+	log.Println("[API] db connected !")
 	// db.LogMode(true)
 	defer db.Close()
+
+	clientOptions := mqtt.NewClientOptions().
+		AddBroker(os.Getenv("LISTEN_PORT")).
+		SetConnectionLostHandler(connLostHandler).
+		SetConnectTimeout(1 * time.Second).
+		SetOnConnectHandler(onConnectHandler)
+
+	mqtt.DEBUG = log.New(os.Stderr, "DEBUG    ", log.Ltime)
+	c := mqtt.NewClient(clientOptions)
+	go mqttConnect(c)
 
 	r := httprouter.New()
 	uc := AuthController.New(db)
 	uc.RegisterRoutes(r)
 	sc := SessionController.New(db)
 	sc.RegisterRoutes(r)
+	mc := MessageController.New(c)
+	mc.RegisterRoutes(r)
 
 	log.Printf("http listening on %s", httpPort)
 	if err := http.ListenAndServe(httpPort, r); err != nil {
 		log.Panic(err)
 	}
+}
+
+func mqttConnect(c mqtt.Client) {
+	i := 0
+	success := false
+	for !success {
+		log.Println(i)
+		token := c.Connect()
+		token.WaitTimeout(5 * time.Second)
+		if token.Wait() && token.Error() != nil {
+			log.Println(token.Error())
+		} else {
+			success = true
+		}
+		if i == 3 {
+			panic(token.Error())
+		}
+		i = i + 1
+	}
+}
+
+func connLostHandler(c mqtt.Client, err error) {
+	log.Printf("Connection lost, reason: %v\n", err)
+	//Perform additional action...
+}
+
+func onConnectHandler(c mqtt.Client) {
+	log.Println("Client Connected")
+	//Perform additional action...
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/ilgianlu/tagyou
 go 1.14
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.2.0
 	github.com/jinzhu/gorm v1.9.15
 	github.com/joho/godotenv v1.3.0
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
-	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
 )
