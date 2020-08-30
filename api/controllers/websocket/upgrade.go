c8bb77c944d2e5694241805f15ae3ad0dc48f5af
Gianluca Magni <gmagni@came.com>
2020-08-30 15:02:35 +0200

    copy websocket messages content into buffer and scans via ws

--- api/controllers/websocket/upgrade.go
+++ api/controllers/websocket/upgrade.go
@@ -1,15 +1,18 @@
 package websocket
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"time"
 
+	"github.com/ilgianlu/tagyou/packet"
 	"github.com/julienschmidt/httprouter"
-	"golang.org/x/time/rate"
 	ws "nhooyr.io/websocket"
 )
 
@@ -21,9 +24,52 @@ func (wc WebsocketController) UpgradeConnection(w http.ResponseWriter, r *http.R
 	}
 	defer c.Close(ws.StatusInternalError, "[WS] the sky is falling")
 
-	l := rate.NewLimiter(rate.Every(time.Second*1), 10)
+	buf := bytes.NewBuffer([]byte{})
+	go readInto(r, c, buf)
+
+	scanner := bufio.NewScanner(buf)
+	packetSplit := func(b []byte, atEOF bool) (advance int, token []byte, err error) {
+		if len(b) == 0 && atEOF {
+			// socket down - closed
+			return 0, b, bufio.ErrFinalToken
+		}
+		pb, err := packet.ReadFromByteSlice(b)
+		if err != nil {
+			log.Printf("[WS] %s\n", err)
+			if !atEOF {
+				return 0, nil, nil
+			}
+			return 0, pb, bufio.ErrFinalToken
+		}
+		return len(pb), pb, nil
+	}
+	scanner.Split(packetSplit)
+
+	for scanner.Scan() {
+		err := scanner.Err()
+		if err != nil {
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				// socket up but silent
+				log.Printf("[WS] socket up but silent %s\n", err)
+			}
+		}
+
+		b := scanner.Bytes()
+		p, err := packet.Start(b)
+		if err != nil {
+			log.Printf("[WS] Start err %s\n", err)
+			return
+		}
+		parseErr := p.Parse()
+		if parseErr != 0 {
+			log.Printf("[WS] parseErr %d\n", parseErr)
+		}
+	}
+}
+
+func readInto(r *http.Request, c *ws.Conn, buf *bytes.Buffer) {
 	for {
-		err = echo(r.Context(), c, l)
+		err := fillBuff(r.Context(), c, buf)
 		if ws.CloseStatus(err) == ws.StatusNormalClosure {
 			return
 		}
@@ -34,31 +80,21 @@ func (wc WebsocketController) UpgradeConnection(w http.ResponseWriter, r *http.R
 	}
 }
 
-func echo(ctx context.Context, c *ws.Conn, l *rate.Limiter) error {
+func fillBuff(ctx context.Context, c *ws.Conn, buf *bytes.Buffer) error {
 	ctx, cancel := context.WithTimeout(ctx, time.Second*100)
 	defer cancel()
-
-	err := l.Wait(ctx)
-	if err != nil {
-		return err
-	}
 
 	typ, r, err := c.Reader(ctx)
 	if err != nil {
 		return err
 	}
+	log.Println("[WS] message type", typ)
 
-	w, err := c.Writer(ctx, typ)
-	if err != nil {
-		return err
-	}
-
-	_, err = io.Copy(w, r)
+	_, err = io.Copy(buf, r)
 	if err != nil {
 		return fmt.Errorf("[WS] failed to io.Copy: %w", err)
 	}
-	log.Println("[WS] echoed")
+	log.Println("[WS] copied")
 
-	err = w.Close()
 	return err
 }
