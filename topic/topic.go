0477f9733e435dbbd6fc740b30cda208a640fd72
Gianluca Magni <gmagni@came.com>
2020-09-21 08:02:37 +0200

    topic explode ok

--- topic/topic.go
+++ topic/topic.go
@@ -1,9 +1,12 @@
 package topic
 
-import "strings"
+import (
+	"strings"
+)
 
 const TOPIC_SEPARATOR = "/"
 const TOPIC_WILDCARD = "#"
+const TOPIC_JOLLY = "+"
 
 func Match(topic string, matcher string) bool {
 	if matcher == "#" || topic == matcher {
@@ -46,4 +49,48 @@ func MatcherSubset(subSet string, set string) bool {
 		}
 	}
 	return true
+}
+
+func Explode(topic string) []string {
+	setRoad := strings.Split(topic, TOPIC_SEPARATOR)
+	res := []string{
+		"#",
+		topic,
+	}
+	if len(setRoad) == 1 {
+		res = append(res, TOPIC_JOLLY)
+		return res
+	}
+	for i := 0; i < len(setRoad); i++ {
+		prev := pre(setRoad, i)
+		pos := post(setRoad, i)
+		if prev == "" {
+			res = append(res, setRoad[i]+TOPIC_SEPARATOR+TOPIC_WILDCARD)
+			res = append(res, TOPIC_JOLLY+TOPIC_SEPARATOR+TOPIC_WILDCARD)
+			res = append(res, TOPIC_JOLLY+TOPIC_SEPARATOR+pos)
+		} else {
+			if pos == "" {
+				res = append(res, prev+TOPIC_SEPARATOR+TOPIC_JOLLY)
+			} else {
+				res = append(res, prev+TOPIC_SEPARATOR+setRoad[i]+TOPIC_SEPARATOR+TOPIC_WILDCARD)
+				res = append(res, prev+TOPIC_SEPARATOR+TOPIC_JOLLY+TOPIC_SEPARATOR+pos)
+				res = append(res, prev+TOPIC_SEPARATOR+TOPIC_JOLLY+TOPIC_SEPARATOR+TOPIC_WILDCARD)
+			}
+		}
+	}
+	return res
+}
+
+func pre(path []string, i int) string {
+	if i == 0 {
+		return ""
+	}
+	return strings.Join(path[0:i], TOPIC_SEPARATOR)
+}
+
+func post(path []string, i int) string {
+	if i == len(path)-1 {
+		return ""
+	}
+	return strings.Join(path[i+1:], TOPIC_SEPARATOR)
 }
