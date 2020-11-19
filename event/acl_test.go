ab698ad468f9b3dd20ab02515b4f206264249bcc
Gianluca Magni <gmagni@came.com>
2020-11-19 16:59:31 +0100

    more tests

--- event/acl_test.go
+++ event/acl_test.go
@@ -1,6 +1,8 @@
 package event
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCheckAcls(t *testing.T) {
 	type test struct {
@@ -17,5 +19,26 @@ func TestCheckAcls(t *testing.T) {
 		if CheckAcl(value.data[0], value.data[1]) != value.result {
 			t.Errorf("expected %s to acl check %s, %t", value.data[0], value.data[1], value.result)
 		}
+	}
+}
+
+func TestReadAcls(t *testing.T) {
+	var acls []Acl
+	var err error
+	acls, err = readAcls("")
+	if err == nil {
+		t.Errorf("expected to err! %s", acls)
+	}
+	acls, err = readAcls("{}")
+	if err == nil {
+		t.Errorf("expected to err! %s", acls)
+	}
+	acls, err = readAcls("[]")
+	if err != nil || len(acls) != 0 {
+		t.Errorf("expected no error and empty acl array! %s", acls)
+	}
+	acls, err = readAcls("[{\"pattern\": \"/a/#\"}]")
+	if err != nil || len(acls) != 1 {
+		t.Errorf("expected no error and one element acl array! %s", acls)
 	}
 }
