f06c111246af871a9778643feaf3cbc7b938eb8e
Gianluca Magni <gmagni@came.com>
2022-01-31 08:45:58 +0100

    session lock for event subscribe

--- event/event_subscribe.go
+++ event/event_subscribe.go
@@ -13,32 +13,41 @@ import (
 func onSubscribe(db *gorm.DB, p *packet.Packet, outQueue chan<- *out.OutData) {
 	reasonCodes := []uint8{}
 	for _, subscription := range p.Subscriptions {
-		rCode := clientSubscription(db, p.Session, &subscription, outQueue)
+		rCode := clientSubscription(db, p.Session, subscription, outQueue)
 		reasonCodes = append(reasonCodes, rCode)
 	}
 	clientSubscribed(p, reasonCodes, outQueue)
 }
 
 func clientSubscribed(p *packet.Packet, reasonCodes []uint8, outQueue chan<- *out.OutData) {
+	p.Session.Mu.RLock()
+	clientId := p.Session.ClientId
+	protocolVersion := p.Session.ProtocolVersion
+	p.Session.Mu.RUnlock()
 	var o out.OutData
-	o.ClientId = p.Session.ClientId
-	o.Packet = packet.Suback(p.PacketIdentifier(), reasonCodes, p.Session.ProtocolVersion)
+	o.ClientId = clientId
+	o.Packet = packet.Suback(p.PacketIdentifier(), reasonCodes, protocolVersion)
 	outQueue <- &o
 }
 
-func clientSubscription(db *gorm.DB, session *model.RunningSession, subscription *model.Subscription, outQueue chan<- *out.OutData) uint8 {
+func clientSubscription(db *gorm.DB, session *model.RunningSession, subscription model.Subscription, outQueue chan<- *out.OutData) uint8 {
+	session.Mu.RLock()
+	fromLocalhost := session.FromLocalhost()
+	subscribeAcl := session.SubscribeAcl
+	protocolVersion := session.ProtocolVersion
+	session.Mu.RUnlock()
 	// check subscr qos, topic valid...
-	if conf.ACL_ON && !session.FromLocalhost() && !CheckAcl(subscription.Topic, session.SubscribeAcl) {
+	if conf.ACL_ON && !fromLocalhost && !CheckAcl(subscription.Topic, subscribeAcl) {
 		return conf.SUB_TOPIC_FILTER_INVALID
 	}
-	db.Create(subscription)
+	db.Create(&subscription)
 	if !subscription.Shared {
-		sendRetain(db, session.ProtocolVersion, subscription, outQueue)
+		sendRetain(db, protocolVersion, subscription, outQueue)
 	}
 	return 0
 }
 
-func sendRetain(db *gorm.DB, protocolVersion uint8, subscription *model.Subscription, outQueue chan<- *out.OutData) {
+func sendRetain(db *gorm.DB, protocolVersion uint8, subscription model.Subscription, outQueue chan<- *out.OutData) {
 	retains := findRetains(db, subscription.Topic)
 	if len(retains) == 0 {
 		return
