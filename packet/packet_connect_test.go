915bc4d5b8514ef1758afd587c82ade1838f504b
Gianluca Magni <gianluca.magni@gmail.com>
2022-06-05 14:30:23 +0200

    first benchmarks for packet start/parse

--- packet/packet_connect_test.go
+++ packet/packet_connect_test.go
@@ -2,9 +2,11 @@ package packet
 
 import (
 	"testing"
+	"time"
 
 	"github.com/ilgianlu/tagyou/conf"
 	"github.com/ilgianlu/tagyou/model"
+	"github.com/rs/zerolog"
 )
 
 func TestConnectSuccess(t *testing.T) {
@@ -44,5 +46,75 @@ func TestConnectSuccessWithProperties(t *testing.T) {
 	}
 	if p.SessionExpiryInterval() != 60 {
 		t.Errorf("expected sessione expiry interval 60 sec, found %d", p.SessionExpiryInterval())
+	}
+}
+
+func BenchmarkStartConnect(b *testing.B) {
+	buffer := []byte{16, 59, 0, 4, 77, 81, 84, 84, 4, 6, 0, 60, 0, 15, 109, 113, 116, 116, 106, 115, 95, 53, 51, 48, 48, 102, 100, 54, 51, 0, 8, 108, 97, 115, 116, 119, 105, 108, 108, 0, 20, 97, 32, 118, 101, 114, 121, 32, 115, 104, 111, 114, 116, 32, 109, 101, 115, 115, 97, 103, 101}
+	for n := 0; n < b.N; n++ {
+		Start(buffer)
+	}
+}
+
+func BenchmarkStartSubscribe(b *testing.B) {
+	buffer := []byte{130, 13, 149, 223, 0, 8, 112, 114, 101, 115, 101, 110, 99, 101, 0}
+	for n := 0; n < b.N; n++ {
+		Start(buffer)
+	}
+}
+
+func BenchmarkStartPublish(b *testing.B) {
+	buffer := []byte{48, 20, 0, 8, 112, 114, 101, 115, 101, 110, 99, 101, 72, 101, 108, 108, 111, 32, 109, 113, 116, 116}
+	for n := 0; n < b.N; n++ {
+		Start(buffer)
+	}
+}
+
+func BenchmarkParseConnect(b *testing.B) {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	buffer := []byte{16, 59, 0, 4, 77, 81, 84, 84, 4, 6, 0, 60, 0, 15, 109, 113, 116, 116, 106, 115, 95, 53, 51, 48, 48, 102, 100, 54, 51, 0, 8, 108, 97, 115, 116, 119, 105, 108, 108, 0, 20, 97, 32, 118, 101, 114, 121, 32, 115, 104, 111, 114, 116, 32, 109, 101, 115, 115, 97, 103, 101}
+	p, _ := Start(buffer)
+
+	session := model.RunningSession{
+		KeepAlive:      conf.DEFAULT_KEEPALIVE,
+		ExpiryInterval: int64(conf.SESSION_MAX_DURATION_SECONDS),
+		LastConnect:    time.Now().Unix(),
+	}
+	p.Session = &session
+
+	for n := 0; n < b.N; n++ {
+		p.Parse()
+	}
+}
+
+func BenchmarkParseSubscribe(b *testing.B) {
+	buffer := []byte{130, 13, 149, 223, 0, 8, 112, 114, 101, 115, 101, 110, 99, 101, 0}
+	p, _ := Start(buffer)
+
+	session := model.RunningSession{
+		KeepAlive:      conf.DEFAULT_KEEPALIVE,
+		ExpiryInterval: int64(conf.SESSION_MAX_DURATION_SECONDS),
+		LastConnect:    time.Now().Unix(),
+	}
+	p.Session = &session
+
+	for n := 0; n < b.N; n++ {
+		p.Parse()
+	}
+}
+
+func BenchmarkParsePublish(b *testing.B) {
+	buffer := []byte{48, 20, 0, 8, 112, 114, 101, 115, 101, 110, 99, 101, 72, 101, 108, 108, 111, 32, 109, 113, 116, 116}
+	p, _ := Start(buffer)
+
+	session := model.RunningSession{
+		KeepAlive:      conf.DEFAULT_KEEPALIVE,
+		ExpiryInterval: int64(conf.SESSION_MAX_DURATION_SECONDS),
+		LastConnect:    time.Now().Unix(),
+	}
+	p.Session = &session
+
+	for n := 0; n < b.N; n++ {
+		p.Parse()
 	}
 }
