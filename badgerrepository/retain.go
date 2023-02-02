7f698db5eb56b130a9e4fdb7816c32e407ec933d
Gianluca Magni <magni.c-@legrand-ext.com>
2023-02-02 14:35:24 +0100

    Retaing badger repository ok

--- badgerrepository/retain.go
+++ badgerrepository/retain.go
@@ -1,9 +1,11 @@
 package badgerrepository
 
 import (
+	"strings"
+
 	"github.com/dgraph-io/badger/v3"
-	"github.com/ilgianlu/tagyou/conf"
 	"github.com/ilgianlu/tagyou/model"
+	"github.com/ilgianlu/tagyou/topic"
 )
 
 const RETAIN_PREFIX = "retain@"
@@ -18,31 +20,33 @@ func RetainKey(topic string) []byte {
 	return k
 }
 
+func TopicFromKey(key []byte) string {
+	return strings.TrimPrefix(string(key), RETAIN_PREFIX)
+}
+
 func RetainValue(retain model.Retain) ([]byte, error) {
 	return model.GobEncode(retain)
 }
 
 func (r RetainBadgerRepository) FindRetains(subscribedTopic string) []model.Retain {
-	trimmedTopic := trimWildcard(subscribedTopic)
 	var retains []model.Retain
-
 	r.Db.View(func(txn *badger.Txn) error {
 		opts := badger.DefaultIteratorOptions
-		opts.PrefetchSize = 10
+		opts.PrefetchValues = false
 		it := txn.NewIterator(opts)
 		defer it.Close()
 		for it.Rewind(); it.Valid(); it.Next() {
 			item := it.Item()
-			err := item.Value(func(v []byte) error {
-				a, err := model.GobDecode[model.Retain](v)
-				if err != nil {
-					return err
-				}
-				retains = append(retains, a)
-				return nil
-			})
-			if err != nil {
-				return err
+			tpc := TopicFromKey(item.Key())
+			if topic.Match(tpc, subscribedTopic) {
+				item.Value(func(val []byte) error {
+					ret, err := model.GobDecode[model.Retain](val)
+					if err != nil {
+						return err
+					}
+					retains = append(retains, ret)
+					return nil
+				})
 			}
 		}
 		return nil
@@ -51,18 +55,19 @@ func (r RetainBadgerRepository) FindRetains(subscribedTopic string) []model.Reta
 }
 
 func (r RetainBadgerRepository) Create(retain model.Retain) error {
-	return r.Db.Create(&retain).Error
+	return r.Db.Update(func(txn *badger.Txn) error {
+		k := RetainKey(retain.Topic)
+		v, err := RetainValue(retain)
+		if err != nil {
+			return err
+		}
+		return txn.Set(k, v)
+	})
 }
 
 func (r RetainBadgerRepository) Delete(retain model.Retain) error {
-	return r.Db.Delete(&retain).Error
-}
-
-func trimWildcard(topic string) string {
-	lci := len(topic) - 1
-	lc := topic[lci]
-	if string(lc) == conf.WILDCARD_MULTI_LEVEL {
-		topic = topic[:lci]
-	}
-	return topic
+	return r.Db.Update(func(txn *badger.Txn) error {
+		k := RetainKey(retain.Topic)
+		return txn.Delete(k)
+	})
 }
